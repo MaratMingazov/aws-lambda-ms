c7f51752228743712d6f7f2c1b90ff05a2eff833
Marat Mingazov <maratmingazovr@gmail.com>
2023-03-29 19:19:37 +0300

    Update list-dragins-ms

--- list-dragons-ms/src/main/java/maratmingazov/ListDragonsApp.java
+++ list-dragons-ms/src/main/java/maratmingazov/ListDragonsApp.java
@@ -2,19 +2,31 @@ package maratmingazov;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 
+import java.util.HashMap;
 import java.util.Map;
 
 /**
  * Hello world!
  *
  */
-public class ListDragonsApp implements RequestHandler<Map<String, String>, String> {
+public class ListDragonsApp implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
 
     @Override
-    public String handleRequest(Map<String, String> stringStringMap,
-                                Context context) {
-        return "helloWorld";
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent input, Context context) {
+        return generateResponse("HelloWorld");
+    }
+
+    private  APIGatewayProxyResponseEvent generateResponse(String dragons) {
+        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
+        Map<String, String> headers = new HashMap<>();
+        headers.put("access-control-allow-origin", "*");
+        response.setStatusCode(200);
+        response.setBody(dragons);
+        response.setHeaders(headers);
+        return response;
     }
 }
