03c731b2088cecddc2c54fe49290200e763620f4
Marat Mingazov <maratmingazovr@gmail.com>
2023-03-30 19:30:32 +0300

    Implement getBucket method

--- list-dragons-ms/src/main/java/maratmingazov/ListDragonsApp.java
+++ list-dragons-ms/src/main/java/maratmingazov/ListDragonsApp.java
@@ -4,6 +4,10 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import software.amazon.awssdk.services.s3.S3AsyncClient;
+import software.amazon.awssdk.services.ssm.SsmClient;
+import software.amazon.awssdk.services.ssm.model.GetParameterRequest;
+import software.amazon.awssdk.services.ssm.model.GetParameterResponse;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -14,10 +18,28 @@ import java.util.Map;
  */
 public class ListDragonsApp implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
+    private final SsmClient ssmClient = SsmClient.builder().build();
+    private final S3AsyncClient s3 = S3AsyncClient.builder().build();
 
     @Override
-    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent input, Context context) {
-        return generateResponse("HelloWorld");
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent event, Context context) {
+        String dragonsData = readDragonData(event);
+        return generateResponse(dragonsData);
+    }
+
+    private String readDragonData(APIGatewayProxyRequestEvent event) {
+        Map<String, String> queryParams = event.getQueryStringParameters();
+        String bucketName = getBucketName();
+        return bucketName;
+    }
+
+    private String getBucketName() {
+        GetParameterRequest getParameterRequest = GetParameterRequest.builder()
+                                                                     .name("dragon_data_bucket_name")
+                                                                     .withDecryption(false)
+                                                                     .build();
+        GetParameterResponse getParameterResponse = ssmClient.getParameter(getParameterRequest);
+        return getParameterResponse.parameter().value();
     }
 
     private  APIGatewayProxyResponseEvent generateResponse(String dragons) {
